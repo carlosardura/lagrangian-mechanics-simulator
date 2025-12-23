16558961058bb4853423b7bde3f965bacfbad015
Carlos Ardura <carlosardura@macbook-air-carlos-ardura.local>
2025-12-23 03:16:09 +0100

    Improve Euler, Rk4 and Verlet integrators

--- integrators.m
+++ integrators.m
@@ -1,119 +1,154 @@
-function [pos, vel] = euler_step(acc, pos0, vel0, t)
-% Explicit Euler method for N dimensions
+function [tp, pos, vel] = eulode_phys(acc, tspan, pos0, vel0, h)
 
-% Input:
-% acc  - acceleration function returning Nx1 vector
-% pos0 - initial position vector (Nx1)
-% vel0 - initial velocity vector (Nx1)
-% t    - vector of times
+if nargin < 5
+    error('At least 5 input arguments required');
+end
 
-% Output:
-% pos  - matrix of positions, rows = times, columns = dimensions
-% vel  - matrix of velocities, rows = times, columns = dimensions
+t0 = tspan(1); tf = tspan(2);
 
-h = t(2) - t(1);        % integration step
-N = length(t);          % number of steps
-dim = length(pos0);     % system dimension
+if any(diff(tspan) <= 0)
+    error('tspan must be strictly increasing');
+end
 
-pos = zeros(N, dim);    % define vectors
-vel = zeros(N, dim);
+tp = (t0:h:tf);
+
+if tp(end) < tf
+    tp(end+1) = tf;
+end
+n_out = length(tp);
+
+dim = length(pos0);       % degrees of freedom
+pos = zeros(n_out, dim);
+vel = zeros(n_out, dim);
 
 pos(1,:) = pos0(:)';    % add initial values
 vel(1,:) = vel0(:)';
 
-for i = 1:N-1
-    x = pos(i,:)';
-    v = vel(i,:)';
-   
-    x_new = x + h * v;
-    v_new = v + h * acc(x, v);
-    
-    pos(i+1,:) = x_new';
-    vel(i+1,:) = v_new';
+tt = t0;
+x = pos0(:);
+v = vel0(:);
+
+for np = 1:n_out-1
+    t_end = tp(np+1);
+    while tt < t_end
+        hh = min(h, t_end - tt);
+        v = v + hh * acc(x, v);
+        x = x + hh * v;
+        tt = tt + hh;
+    end
+    pos(np+1,:) = x';
+    vel(np+1,:) = v';
+end
 end
 
+
+function [tp, pos, vel] = rk4sys_phys(acc, tspan, pos0, vel0, h)
+
+if nargin < 5
+    error('At least 5 input arguments required');
 end
 
+if any(diff(tspan) <= 0)
+    error('tspan not in ascending order');
+end
 
-function [pos, vel] = rk4_step(acc, pos0, vel0, t)
-% 4th-order Runge-Kutta method for N dimensions
+dim = length(pos0);   % degrees of freedom
+t0 = tspan(1); tf = tspan(end);
 
-% Input:
-% acc  - acceleration function returning Nx1 vector
-% pos0 - initial position vector (Nx1)
-% vel0 - initial velocity vector (Nx1)
-% t    - vector of times
+if length(tspan) == 2
+    tp = (t0:h:tf)';
+    if tp(end) < tf
+        tp(end+1) = tf;
+    end
+else
+    tp = tspan(:);
+end
 
-% Output:
-% pos  - matrix of positions, rows = times, columns = dimensions
-% vel  - matrix of velocities, rows = times, columns = dimensions
-
-h = t(2) - t(1);        % integration step
-N = length(t);          % number of steps
-dim = length(pos0);     % system dimension
-
-pos = zeros(N, dim);    % define vectors
-vel = zeros(N, dim);
+npos = length(tp);
+pos = zeros(npos, dim);
+vel = zeros(npos, dim);
 
 pos(1,:) = pos0(:)';    % add initial values
 vel(1,:) = vel0(:)';
 
-for i = 1:N-1
-    x = pos(i,:)';
-    v = vel(i,:)';
-    
-    k1x = v;
-    k1v = acc(x, v);
-    
-    k2x = v + h/2*k1v;
-    k2v = acc(x + h/2*k1x, v + h/2*k1v);
-    
-    k3x = v + h/2*k2v;
-    k3v = acc(x + h/2*k2x, v + h/2*k2v);
-    
-    k4x = v + h*k3v;
-    k4v = acc(x + h*k3x, v + h*k3v);
-    
-    pos(i+1,:) = (x + h/6*(k1x + 2*k2x + 2*k3x + k4x))';
-    vel(i+1,:) = (v + h/6*(k1v + 2*k2v + 2*k3v + k4v))';
+tt = t0;
+x = pos0(:);
+v = vel0(:);
+
+np = 1;    % time vector index
+
+while tt < tf
+    t_target = tp(np+1);
+    while tt < t_target
+        hh = min(h, t_target - tt);
+
+        k1x = v;
+        k1v = acc(x, v);
+
+        k2x = v + hh/2 * k1v;
+        k2v = acc(x + hh/2 * k1x, v + hh/2 * k1v);
+
+        k3x = v + hh/2 * k2v;
+        k3v = acc(x + hh/2 * k2x, v + hh/2 * k2v);
+
+        k4x = v + hh * k3v;
+        k4v = acc(x + hh * k3x, v + hh * k3v);
+
+        x = x + hh/6 * (k1x + 2*k2x + 2*k3x + k4x);
+        v = v + hh/6 * (k1v + 2*k2v + 2*k3v + k4v);
+        tt = tt + hh;
+    end
+    np = np + 1;
+    tp(np) = tt;
+    pos(np,:) = x';
+    vel(np,:) = v';
 end
 end
 
 
-function [pos, vel] = verlet_step(acc, pos0, vel0, t)
-% Verlet Integration method for N dimensions
+function [tp, pos, vel] = velverlet_phys(acc, tspan, pos0, vel0, h)
 
-% Input:
-% acc  - acceleration function returning Nx1 vector
-% pos0 - initial position vector (Nx1)
-% vel0 - initial velocity vector (Nx1)
-% t    - vector of times
+if nargin < 5
+    error('At least 5 input arguments required');
+end
 
-% Output:
-% pos  - matrix of positions, rows = times, columns = dimensions
-% vel  - matrix of velocities, rows = times, columns = dimensions
+t0 = tspan(1); tf = tspan(2);
 
-h = t(2) - t(1);        % integration step
-N = length(t);          % number of steps
-dim = length(pos0);     % system dimension
+if any(diff(tspan) <= 0)
+    error('tspan must be strictly increasing');
+end
 
-pos = zeros(N, dim);    % define vectors
-vel = zeros(N, dim);
+tp = (t0:h:tf);
+if tp(end) < tf
+    tp(end+1) = tf;
+end
 
-pos(1,:) = pos0(:)';    % add initial values
+n_out = length(tp);
+dim = length(pos0);
+pos = zeros(n_out, dim);
+vel = zeros(n_out, dim);
+
+pos(1,:) = pos0(:)';
 vel(1,:) = vel0(:)';
-a = acc(pos0, vel0);
 
-for i = 1:N-1
-    x = pos(i,:)';
-    v = vel(i,:)';
-    
-    x_new = x + v * h + 0.5 * a * h^2;
-    a_new = acc(x_new, v);
-    v_new = v + 0.5 * (a + a_new) * h;
+tt = t0;
+x = pos0(:);
+v = vel0(:);
 
-    pos(i+1,:) = x_new';
-    vel(i+1,:) = v_new';
-    a = a_new;
+for np = 1:n_out-1
+    t_end = tp(np+1);
+    while tt < t_end
+        hh = min(h, t_end - tt);
+        a0 = acc(x, v);
+        x_new = x + v * hh + 0.5 * a0 * hh^2;
+        a_new = acc(x_new, v);
+        v_new = v + 0.5 * (a0 + a_new) * hh;
+        x = x_new;
+        v = v_new;
+        tt = tt + hh;
+    end
+    np = np + 1;
+    pos(np,:) = x';
+    vel(np,:) = v';
 end
 end
